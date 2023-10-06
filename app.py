cfe2086f16f95d92d58b3a81ebb155a9cbac5988
Justin Huynh <justinhuynh639@gmail.com>
2023-10-06 12:26:33 -0700

    added dictionary to break if empty

--- app.py
+++ app.py
@@ -4,20 +4,10 @@ from PyDictionary import PyDictionary
 
 dictionary = PyDictionary()
 
-# have a python dictionary that has a key, value pair that represents a word and it's definition
-def main():
-    word_dictionary = {
-        'hi': 'a way of greeting',
-        'eyes': 'an organ for seeing',
-        'earth': 'a planet in space',
-    }
-
-    # collect input from the user, input is a word
-    while True:
-        word = input('Enter a word: ')
-        if word == '':
-            break
-        if word in word_dictionary:
-            print(word, ':', word_dictionary[word])
-
-main()
+#ask user for what it wants to get the defintion of
+while True:
+    word = input('Enter your word: ')
+    if word == '':
+        break
+    
+    print(dictionary.meaning(word))
