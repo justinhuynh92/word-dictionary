9d9fd3e7e005982a37e4de37ea19036e51b72075
Justin Huynh <justinhuynh639@gmail.com>
2023-10-06 12:18:46 -0700

    started word dictionary

---
+++ app.py
@@ -0,0 +1,20 @@
+# check if the word is in the dictionary
+# print the definition
+
+# have a python dictionary that has a key, value pair that represents a word and it's definition
+def main():
+    word_dictionary = {
+        'hi': 'a way of greeting',
+        'eyes': 'an organ for seeing',
+        'earth': 'a planet in space',
+    }
+
+    # collect input from the user, input is a word
+    while True:
+        word = input('Enter a word: ')
+        if word == '':
+            break
+        if word in word_dictionary:
+            print(word, ':', word_dictionary[word])
+
+main()
